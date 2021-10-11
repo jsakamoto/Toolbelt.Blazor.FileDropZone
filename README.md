1c75f4b31c69c96d2d5cc76a17b12e815a07cb25
jsakamoto <junichi.jp@gmail.com>
2021-10-11 22:42:38 +0900

    Deploy the sample site to GitHub Pages.

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Surround an "input type=file" element by this Blazor component to making a zone that accepts drag and drops files.
 
+- Live demo site - https://jsakamoto.github.io/Toolbelt.Blazor.FileDropZone/
+
 ## Release Note
 
 [Release notes](https://github.com/jsakamoto/Toolbelt.Blazor.FileDropZone/blob/master/RELEASE-NOTES.txt)
--- SampleSite/Program.cs
+++ SampleSite/Program.cs
@@ -15,9 +15,14 @@ namespace SampleSite
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddMudServices();
-            builder.Services.AddI18nText();
+            ConfigureServices(builder.Services);
             await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMudServices();
+            services.AddI18nText();
         }
     }
 }
