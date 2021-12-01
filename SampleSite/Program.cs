95f358fc1b4a1076cc78662a08f6217da653aa5c
jsakamoto <junichi.jp@gmail.com>
2021-12-01 16:03:49 +0900

    Upgrade the sample site to .NET 6

--- SampleSite/Program.cs
+++ SampleSite/Program.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using SampleSite;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-namespace SampleSite
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            ConfigureServices(builder.Services);
-            await builder.Build().RunAsync();
-        }
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services);
 
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMudServices();
-            services.AddI18nText();
-        }
-    }
+await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddMudServices();
+    services.AddI18nText();
 }
